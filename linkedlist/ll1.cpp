9fbb92ebee5b109f22a809c71e3a4259dad1eda5
Arun-2010 <arunbandari15@gmail.com>
2025-09-06 09:42:27 +0530

    ll

--- linkedlist/ll1.cpp
+++ linkedlist/ll1.cpp
@@ -1,101 +1,136 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
 
-class node{
-    public:
+class node {
+public:
     int data;
-    node*next;
-    
-    node(int data){
-        this->data=data;
-        this->next=NULL;
+    node* next;
+
+    node(int data) {
+        this->data = data;
+        this->next = NULL;
     }
 };
 
-void printll(node*head){
-    node*temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<"->";
-        temp=temp->next;
+int getlength(node* head) {
+    int c = 0;
+    while (head != NULL) {
+        c++;
+        head = head->next;
     }
-    
+    return c;
 }
 
-
-void insertATbe(node*&head,node*&tail,int data){
-    if(head==NULL){
-        // empty linked list
-        node*newnode=new node(data);
-        head=newnode;
-        tail=newnode;
+void printll(node* head) {
+    node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
     }
-    else{
-        node*newnode= new node(data);
-        newnode->next=head;
-        head=newnode;
-    }
-    
+    cout << "\n";
 }
 
-
-void insertATmid(){
-    
-}
-
-
-
-void insertATbeend(node*&head,node*&tail,int data){
-    if(head==NULL){
-        node*newnode=new node(data);
-        head=newnode;
-        tail=newnode;
-    }else{
-        node*newnode=new node(data);
-        tail->next=newnode;
-        tail=newnode;
+void insertATbe(node*& head, node*& tail, int data) {
+    node* newnode = new node(data);
+    if (head == NULL) {
+        head = tail = newnode;
+    } else {
+        newnode->next = head;
+        head = newnode;
     }
-    
 }
+
+void insertATbeend(node*& head, node*& tail, int data) {
+    node* newnode = new node(data);
+    if (head == NULL) {
+        head = tail = newnode;
+    } else {
+        tail->next = newnode;
+        tail = newnode;
+    }
+}
+
+void insertATmid(node*& head, node*& tail, int data, int position) {
+    int length = getlength(head);
+
+    if (position < 1 || position > length + 1) {
+        cout << "\nInvalid position!\n";
+        return;
+    }
+
+    if (position == 1) {
+        insertATbe(head, tail, data);
+        return;
+    }
+
+    if (position == length + 1) {
+        insertATbeend(head, tail, data);
+        return;
+    }
+
+    node* newnode = new node(data);
+    node* curr = head;
+
+    // Move curr to (position - 1)th node
+    for (int i = 1; i < position - 1; i++) {
+        curr = curr->next;
+    }
+
+    newnode->next = curr->next;
+    curr->next = newnode;
+}
+
+void deleteList(node*& head) {
+    node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
 int main() {
+    node* first = new node(10);
+    node* second = new node(20);
+    node* third = new node(30);
+    node* fourth = new node(40);
 
-node*first=new node(10);
-node*second=new node(20);
-node*third=new node(30);
-node*fourth=new node(40);
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
 
-first->next=second;
-second->next=third;
-third->next=fourth;
+    node* head = first;
+    node* tail = fourth;
 
-node*head=first;
-node*tail=fourth;
-
-
-    cout << "Before insert at beginning:";
+    cout << "Before insert at beginning: ";
     printll(head);
 
-   insertATbe(head, tail, 5);
-
-    cout << "\n After insert at beginning:";
+    insertATbe(head, tail, 68);
+    cout << "After insert at beginning: ";
     printll(head);
 
-    cout << "\n Before insert at end:";
+    cout << "Before insert at end: ";
     printll(head);
 
-   insertATbeend(head, tail, 10);
-
-    cout << "\n After insert at end:";
+    insertATbeend(head, tail, 58);
+    cout << "After insert at end: ";
     printll(head);
 
+    cout << "Position case before: ";
+    printll(head);
 
+    cout << "Position case after: ";
+    insertATmid(head, tail, 244, 3);
+    printll(head);
 
+    // Free memory
+    deleteList(head);
+    return 0;
 }
 
-
- /*output:
-Before insert at beginning:10->20->30->40->
- After insert at beginning:5->10->20->30->40->
- Before insert at end:5->10->20->30->40->
- After insert at end:5->10->20->30->40->10->
- */
+ /*Before insert at beginning: 10 -> 20 -> 30 -> 40 -> 
+After insert at beginning: 68 -> 10 -> 20 -> 30 -> 40 -> 
+Before insert at end: 68 -> 10 -> 20 -> 30 -> 40 -> 
+After insert at end: 68 -> 10 -> 20 -> 30 -> 40 -> 58 -> 
+Position case before: 68 -> 10 -> 20 -> 30 -> 40 -> 58 -> 
+Position case after: 68 -> 10 -> 244 -> 20 -> 30 -> 40 -> 58 -> */
