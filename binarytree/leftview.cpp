1f549129d3bb514b79e953f8c94c83248078c346
Arun-2010 <arunbandari15@gmail.com>
2025-10-18 22:23:42 +0530

    c

--- binarytree/leftview.cpp
+++ binarytree/leftview.cpp
@@ -21,3 +21,36 @@ public:
         return result;
     }
 };
+
+
+
+
+
+
+
+
+// right view of binary tree
+class Solution {
+public:
+    // Helper function
+    void rightViewHelper(Node* root, int level, vector<int>& result) {
+        if (root == NULL) return;
+
+        // If this is the first node at this level
+        if (level == result.size()) {
+            result.push_back(root->data);
+        }
+
+        // Visit right first, then left
+        rightViewHelper(root->right, level + 1, result);
+        rightViewHelper(root->left, level + 1, result);
+        
+    }
+
+    // Main function
+    vector<int> rightView(Node *root) {
+        vector<int> result;
+        rightViewHelper(root, 0, result);
+        return result;
+    }
+};
