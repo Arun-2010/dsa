119be819540dd0c133b4482a311cdb3032b09393
Arun-2010 <arunbandari15@gmail.com>
2026-05-08 12:40:28 +0530

    hash

--- map/map.cpp
+++ map/map.cpp
@@ -1,34 +1,47 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#include<unordered_map>
 
 int main() {
-   //creation
-   unordered_map<string,int>mapping;
-   
-   //insertion
-   pair<string,int>p("arun",21);
-   mapping.insert(p);
-   
-   cout<<"size of map:"<<mapping.size()<<endl;
-   cout<<"value:"<<mapping.at("arun")<<endl;
-   
-   //searching if present 1 else 0
-   cout<<mapping.count("arun")<<endl;
-   cout<<mapping.count("a")<<endl;
-   //better way for searching
-   if(mapping.find("arun")!=mapping.end()){
-       cout<<"found";
-   }
-   else{
-       cout<<"not found";
-   }
-   }
-   
-//output:
- size of map:1
-value:21
+	// your code goes here
+	unordered_map<string,int>mapping;
+	
+	//insert
+	
+	pair<string,int>q("arun",24);
+	
+	
+	
+	mapping.insert(q);
+	
+	cout<<"size of map: "<<mapping.size()<<endl;
+	
+	//acess
+	cout<<mapping.at("arun")<<endl;
+	cout<<mapping["arun"]<<endl;
+	
+	//note using square brackets we can access the elment but if the element doesn't present then it will create an entry 
+	
+	
+	
+	
+	//searching 
+    cout<<mapping.count("arun")<<endl; // count return 1 if it exsists in the map or else return 0;
+    
+    
+    
+    //another way of searching
+    
+    if(mapping.find("arun")!=mapping.end()){
+        cout<<"found";
+    }else{
+        cout<<"not found";
+    }
+}
+
+
+//output
+size of map: 1
+24
+24
 1
-0
 found
