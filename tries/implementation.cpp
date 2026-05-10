3303a10be467874182e6838429a664075b000a4a
Arun-2010 <arunbandari15@gmail.com>
2026-05-10 17:38:24 +0530

    trie

---
+++ tries/implementation.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class TrieNode{
+    public:
+    char value;
+    TrieNode*children[26];
+    bool isTerminal;
+    
+    TrieNode(char val){
+        this->value=val;
+        for(int i=0;i<26;i++){
+            children[i]=NULL;
+        }
+        this->isTerminal=false;
+        
+    }
+};
+
+
+//insertion
+void insertword(TrieNode*root, string word){
+    cout<<"recieved word:"<<word<<endl;
+    if(word.length()==0){
+        root->isTerminal=true;
+        return;
+    }
+    
+    
+    char ch=word[0];
+    int index=ch-'a';
+    TrieNode*child;
+    if(root->children[index] != NULL){
+        
+        //present 
+        child=root->children[index];
+    }
+    else{
+        child=new TrieNode(ch);
+        root->children[index]=child;
+    }
+    insertword(child,word.substr(1));
+    
+}
+
+
+bool searchword(TrieNode*root,string word){
+    if(word.length()==0){
+        return root->isTerminal;
+    }
+    
+    char ch=word[0];
+    int index=ch-'a';
+    TrieNode*child;
+    
+    if(root->children[index]!=NULL){
+        //found
+        child=root->children[index];
+    }else{
+        return false;
+    }
+    
+    //recursion will slove
+    bool recursionAns=searchword(child,word.substr(1));
+    return recursionAns;
+    
+}
+
+int main() {
+	// your code goes here
+	TrieNode*root=new TrieNode('-');
+	
+	insertword(root,"donation");
+	
+	insertword(root,"time");
+	
+	if(searchword(root,"time")){
+	    cout<<"found";
+	}
+	else{
+	    cout<<"not found";
+	}
+	
+	
+	
+	
+
+}
+
+
+//output
+recieved word:donation
+recieved word:onation
+recieved word:nation
+recieved word:ation
+recieved word:tion
+recieved word:ion
+recieved word:on
+recieved word:n
+recieved word:
+recieved word:time
+recieved word:ime
+recieved word:me
+recieved word:e
+recieved word:
+not found
